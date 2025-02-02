d6036a5a0422e77c2358aa62650c654643895416
zeratheuz <zeratheuz@gmail.com>
2025-02-02 10:24:42 -0300

    Update with exercise 03_reverseString solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reverseString = []
+  for (char of string) {
+    reverseString.unshift(char)
+  }
+  return reverseString.join('')
 };
 
 // Do not edit below this line
