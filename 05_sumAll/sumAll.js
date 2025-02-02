8ca1a6e5f115907459d9eaa5ffc5d36d06080d9b
zeratheuz <zeratheuz@gmail.com>
2025-02-02 17:22:29 -0300

    Update with exercise 05_sumAll solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0) return 'ERROR'
+  if (parseInt(num1) !== num1 
+  || parseInt(num2) !== num2 ) return 'ERROR'
+  if (typeof num1 !== 'number' 
+  || typeof num2 !== 'number' ) return 'ERROR'
+  
+  let sum = 0
+  if (num1 < num2) {
+    while (num1 <= num2) {
+      sum += num1++
+    }
+    return sum
+  } else {
+    while (num2 <= num1) {
+      sum += num2++
+    }
+    return sum
+  }
+
 
 };
 
