76bae924c5b03c79647ba17c3e24cadba31f3def
zeratheuz <zeratheuz@gmail.com>
2025-02-02 16:21:53 -0300

    Update with exercise 04_removeFromArray solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array = [], ...removedItems) {
+  for (removedItem of removedItems) {
+    while (array.includes(removedItem)) {
+      array.splice(array.indexOf(removedItem), 1)
+    }
+  }
+  return array
 };
 
 // Do not edit below this line
