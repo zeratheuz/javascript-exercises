a86fe444388f196893317c54ba9b6e3e88fc34d3
zeratheuz <zeratheuz@gmail.com>
2025-02-21 11:44:34 -0300

    Update with exercise 10_fibonacci solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n = 1) {
+  let fibonacci = 0
+  if (n < 0) {fibonacci = "OOPS"}
+  let first = 0
+  let last = 1
+  for(let i = 0; i < +n; ++i) {
+    first = last
+    last = fibonacci
+    fibonacci = first + last
+  }
+  return fibonacci
 };
 
 // Do not edit below this line
