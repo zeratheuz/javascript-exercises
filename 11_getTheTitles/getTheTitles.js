7e2ab7a68e4d3691e5c77533db373fb821c7eebb
zeratheuz <zeratheuz@gmail.com>
2025-02-21 16:19:37 -0300

    Update with exercise 11_getTheTitles solved

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) { 
+  return books.reduce((titles, book) => (titles.push(book.title),  titles),[])
 };
 
 // Do not edit below this line
