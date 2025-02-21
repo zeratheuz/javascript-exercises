d0a6b4489c42fce0a668add0c35076609585c009
zeratheuz <zeratheuz@gmail.com>
2025-02-21 09:47:33 -0300

    Update with exercise 09_palindromes solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string = "") {
+  // const forward = string.slice().replaceAll(' ', '').replaceAll(',', '').replaceAll('!', '').replaceAll('?', '').replaceAll('.', '').toLowerCase()
+  const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789'
+  const forward = string
+  .toLowerCase()
+  .split('')
+  .filter((array) => alphanum.includes(array)).join('')
+  const backward = forward.split('').reverse().join('')
+  return forward === backward
 };
+
+palindromes()
 
 // Do not edit below this line
 module.exports = palindromes;
