cbba47a7f292c5bd8f943af698abeefccbb9f224
zeratheuz <zeratheuz@gmail.com>
2025-02-22 10:39:59 -0300

    Update with exercise 12_findTheOldest solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((oldestPerson, currentPerson) => {
 
+    currentPerson.yearOfDeath ??= new Date().getFullYear()
+
+    if (Object.keys(oldestPerson).length === 0) {
+      (oldestPerson = currentPerson)
+      
+    } else {
+      const actualYear = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth
+      const currentYear = currentPerson.yearOfDeath - currentPerson.yearOfBirth
+
+      if (actualYear < currentYear) { oldestPerson = currentPerson }
+    }
+
+    return oldestPerson
+  }, {})
 };
 
 // Do not edit below this line
